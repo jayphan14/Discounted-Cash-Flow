304a839a3742b1db71652809f452ea085bbc9666
jayphan14 <kha21052004@gmail.com>
2023-01-19 13:57:49 -0500

    Change App.js file

--- frontend/dcf/src/App.js
+++ frontend/dcf/src/App.js
@@ -1,7 +1,12 @@
+import MainPage from "./components/MainPage";
+import NavBar from "./components/NavBar";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>  
+      <NavBar/>
+      <MainPage/>
+    </>
+    
   )
 }
